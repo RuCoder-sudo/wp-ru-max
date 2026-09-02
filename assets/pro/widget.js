7d2066f3cf1b02a3214e905da3871111c19bbfba
Сергей <78095833+RuCoder-sudo@users.noreply.github.com>
2026-09-02 15:26:00 +0300

    Update widget.js

--- assets/pro/widget.js
+++ assets/pro/widget.js
@@ -1,4 +1,4 @@
-/* WP Ru-max PRO frontend v1.0.57. */
+/* WP Ru-max PRO frontend v1.0.58. */
 (function () {
   var menu = document.getElementById('wp-ru-max-pro-menu');
   var icon = document.getElementById('wp-ru-max-icon');
