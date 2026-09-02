15ab984ac1198a53b1e14c8a0576fea790b8bf8b
Сергей <78095833+RuCoder-sudo@users.noreply.github.com>
2026-09-02 13:52:03 +0300

    Update uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -7,7 +7,9 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 // Загружаем классы из папки текущей установки. Это также позволяет удалить
 // старую неполную установку без fatal error из-за отсутствующего автопостинга.
 $wp_ru_max_includes_dir = __DIR__ . '/includes/';
-require_once $wp_ru_max_includes_dir . 'class-wp-ru-max-auto-posting.php';
+if ( is_readable( $wp_ru_max_includes_dir . 'class-wp-ru-max-auto-posting.php' ) ) {
+    require_once $wp_ru_max_includes_dir . 'class-wp-ru-max-auto-posting.php';
+}
 require_once $wp_ru_max_includes_dir . 'class-wp-ru-max.php';
 
 WP_Ru_Max::uninstall();
