4cf5dad715975955b44664da0c06113db188f321
Сергей <78095833+RuCoder-sudo@users.noreply.github.com>
2026-09-05 18:06:08 +0300

    Update admin.js

--- assets/pro/admin.js
+++ assets/pro/admin.js
@@ -1,4 +1,4 @@
-/* WP Ru-max PRO admin v1.0.59. */
+/* WP Ru-max PRO admin v1.0.61. */
 jQuery(function ($) {
   var liveChatPane = $('.wprmp-pane[data-wprmp-pane="livechat"]');
   if (liveChatPane.length && !liveChatPane.find('.wprmp-livechat-conversations').length) liveChatPane.append('<div class="wprmp-livechat-conversations"></div>');
