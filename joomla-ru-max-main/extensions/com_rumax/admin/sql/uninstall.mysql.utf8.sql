c8f1f9b8882885a8f6828249cbb878e5da30207a
Сергей <78095833+RuCoder-sudo@users.noreply.github.com>
2026-09-06 22:32:46 +0300

    Delete joomla-ru-max-main directory

--- joomla-ru-max-main/extensions/com_rumax/admin/sql/uninstall.mysql.utf8.sql
+++
@@ -1,5 +0,0 @@
-DROP TABLE IF EXISTS `#__rumax_conversations`;
-DROP TABLE IF EXISTS `#__rumax_post_meta`;
-DROP TABLE IF EXISTS `#__rumax_history`;
-DROP TABLE IF EXISTS `#__rumax_queue`;
-DROP TABLE IF EXISTS `#__rumax_settings`;
